90640fe0fac13a7c88098cc63d9988d5a7b92125
John Wood <kb2vhn@gmail.com>
2026-07-03 21:58:59 -0400

    add more

--- 000_rough_cad.sql
+++ 000_rough_cad.sql
@@ -16,5 +16,22 @@ CREATE TABLE agencies (
 	named_rank VARCHAR(20), -- rank like major, sheriff, chief
 	website VARCHAR(200), -- example www.somecountySheriff.gov
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	-- updated_by from users table
 );
 
+
+------------------------------------
+-- Users / Dispatchers
+------------------------------------
+
+CREATE TABLE users (
+	user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	agency_id UUID REFERENCES agencies(agency_id),
+	username VARCHAR(50) UNIQUE NOT NULL,
+	password_hash TEXT NOT NULL,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	role VARCHAR(50), -- Dispatcher, Supervisor, Adin, Audit
+	active BOOLEAN DEFAULT TRUE,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
